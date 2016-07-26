45e344bfe5e6db95c7748c86ea726e2c27219637
Wojtek Erbetowski <wojtek@growbots.com>
2016-07-26 07:32:01 +0200

    Simplified app

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import os
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  return "Hello all"
 
