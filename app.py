ce67be96c753003f1f751aa2e264841473225e4e
Wojciech Erbetowski <wojtek@erbetowski.pl>
2016-10-04 07:32:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,5 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def coTamSlychacZielonyLudku():
-  return "Hello all"
-
-
-################################################## its a well commneted line
-# and a well commented code
-#@app.route('/goodbye')
-#def index_bye():
-#  return "good bye"
-
+def hello():
+  return "Hello"
