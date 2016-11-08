e2d82b90ad6b78668c013572a534ba3c9d120c35
Geoffrey Vedernikoff <yefim323@gmail.com>
2016-11-07 16:40:21 -0800

    Fix style

--- app.py
+++ app.py
@@ -4,20 +4,27 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:////tmp/flask_app.db')
+
+DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:////tmp/flask_app.db')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 db = SQLAlchemy(app)
+
 
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(100))
   email = db.Column(db.String(100))
+
   def __init__(self, name, email):
     self.name = name
     self.email = email
 
+
 @app.route('/')
 def index():
-  return render_template('index.html', users = User.query.all())
+  return render_template('index.html', users=User.query.all())
+
 
 @app.route('/user', methods=['POST'])
 def user():
@@ -29,5 +36,5 @@ def user():
 
 if __name__ == '__main__':
   db.create_all()
-  port = int(os.environ.get('PORT',5000))
+  port = int(os.environ.get('PORT', 5000))
   app.run(host='0.0.0.0', port=port)
